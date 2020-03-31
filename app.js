15e099928851233107119ed22df46be1c65fc99d
michaelleriche <mikeleriche@gmail.com>
2020-03-31 17:27:26 -0400

    nav working

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('nav-links');
+    const nav = document.querySelector('.nav-links');
     
-    // const navA = document.querySelector('.nav-links-a');
-    // const navB = document.querySelector('.nav-links-b');
-    // const navC = document.querySelector('.nav-links-c');
-    // const navD = document.querySelector('.nav-links-d');
-    // const navE = document.querySelector('.nav-links-e');
-
-
     const navLinks = document.querySelectorAll('.nav-links li')
     burger.addEventListener('click', () => {
-        document.querySelector('nav a').classList.toggle('nav-active');
+        nav.classList.toggle('nav-active');
         navLinks.forEach((link, index) => {
             if(link.style.animation){
                 link.style.animation = ''
@@ -21,61 +14,18 @@ const navSlide = () => {
         });
         burger.classList.toggle('toggle');
     });
-    // navA.addEventListener('click', () => {
-    //     nav.classList.toggle('nav-active');
-    //     navLinks.forEach((link, index) => {
-    //         if(link.style.animation){
-    //             link.style.animation = ''
-    //         } else {
-    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-    //         }
-    //     });
-    //     burger.classList.toggle('toggle');
-    // });
-    // navB.addEventListener('click', () => {
-    //     nav.classList.toggle('nav-active');
-    //     navLinks.forEach((link, index) => {
-    //         if(link.style.animation){
-    //             link.style.animation = ''
-    //         } else {
-    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-    //         }
-    //     });
-    //     burger.classList.toggle('toggle');
-    // });
-    // navC.addEventListener('click', () => {
-    //     nav.classList.toggle('nav-active');
-    //     navLinks.forEach((link, index) => {
-    //         if(link.style.animation){
-    //             link.style.animation = ''
-    //         } else {
-    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-    //         }
-    //     });
-    //     burger.classList.toggle('toggle');
-    // });
-    // navD.addEventListener('click', () => {
-    //     nav.classList.toggle('nav-active');
-    //     navLinks.forEach((link, index) => {
-    //         if(link.style.animation){
-    //             link.style.animation = ''
-    //         } else {
-    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-    //         }
-    //     });
-    //     burger.classList.toggle('toggle');
-    // });
-    // navE.addEventListener('click', () => {
-    //     nav.classList.toggle('nav-active');
-    //     navLinks.forEach((link, index) => {
-    //         if(link.style.animation){
-    //             link.style.animation = ''
-    //         } else {
-    //             link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
-    //         }
-    //     });
-    //     burger.classList.toggle('toggle');
-    // });
+
+    document.querySelector('nav').addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.4}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
 
     burger.addEventListener('keydown', () => {
         nav.classList.toggle('nav-active');
@@ -151,3 +101,5 @@ AOS.init({
         return window.innerWidth < maxWidth;
     }
 });
+
+
