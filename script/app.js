796f046908cabaaaa812ef5c5904d734e5c499e0
michaelleriche <mikeleriche@gmail.com>
2020-04-15 22:42:02 -0400

    v2 desktop done

--- script/app.js
+++ script/app.js
@@ -105,3 +105,59 @@ AOS.init({
 });
 
 
+let $mouseX = 0, $mouseY = 0;
+let $xp = 0, $yp =0;
+
+$(document).mousemove(function(e){
+    $mouseX = e.pageX;
+    $mouseY = e.pageY;    
+});
+
+let $loop = setInterval(function(){
+$xp += (($mouseX - $xp)/5);
+$yp += (($mouseY - $yp)/5);
+$("#follow-cursor").css({left:$xp +'px', top:$yp +'px'});  
+}, 30);
+
+
+
+
+let img = $('.point');
+if(img.length > 0){
+    let offset = img.offset();
+    function mouse(evt){
+        let center_x = (offset.left) + (img.width()/2);
+        let center_y = (offset.top) + (img.height()/2);
+        let mouse_x = evt.pageX; var mouse_y = evt.pageY;
+        let radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
+        let degree = (radians * (180 / Math.PI) * -1) + 90; 
+        img.css('-moz-transform', 'rotate('+degree+'deg)');
+        img.css('-webkit-transform', 'rotate('+degree+'deg)');
+        img.css('-o-transform', 'rotate('+degree+'deg)');
+        img.css('-ms-transform', 'rotate('+degree+'deg)');
+    }
+    $(document).mousemove(mouse);
+}
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
